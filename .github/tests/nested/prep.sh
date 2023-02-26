6e363f743b1e4aff3026fafa0cbc5cd97ad37e0b
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-02-25 17:24:04 -0800

    Fix tests
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/nested/prep.sh
+++ .github/tests/nested/prep.sh
@@ -4,8 +4,8 @@ kubectl create namespace "${VALUES}-deps"
 helm install -n "${VALUES}-deps" spire charts/spire --wait -f $SCRIPT_DIR/deps-values.yaml
 kubectl get all -n "${VALUES}-deps"
 while true; do
-	bundle=$(kubectl get configmap spire-bundle -o go-template='{{ index .data "bundle.crt" }}')
-	[ "x$bundle" != "x" ] && break
+	kubectl get configmap spire-bundle -o go-template='{{ index .data "bundle.crt" }}' > bundle
+	[ $? -eq 0 ] && break
 	sleep 1;
 done
-kubectl create configmap -n "$VALUES" spire-bundle-upstream --from-literal="bundle.crt=$bundle"
+kubectl create configmap -n "$VALUES" spire-bundle-upstream --from-file=bundle.crt=bundle
