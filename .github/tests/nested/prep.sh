cf39d5d30f8359911a4907fb85088781b89edf24
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-02-25 16:00:31 -0800

    Fix tests
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/nested/prep.sh
+++ .github/tests/nested/prep.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-helm install -n "${VALUES}-deps" charts/spire -f $SCRIPT_DIR/deps.yaml
+kubectl create namespace "${VALUES}-deps"
+helm install -n "${VALUES}-deps" spire charts/spire -f $SCRIPT_DIR/deps.yaml
 kubectl get all -n "${VALUES}-deps"
