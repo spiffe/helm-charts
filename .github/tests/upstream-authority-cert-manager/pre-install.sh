f40b2ae91716c2d54c33730f03aa0e30c141aba0
kfox1111 <Kevin.Fox@pnnl.gov>
2023-03-03 07:02:52 -0800

    Apply suggestions from code review
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>
    
    Co-authored-by: Marco Franssen <marco.franssen@gmail.com>
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/upstream-authority-cert-manager/pre-install.sh
+++ .github/tests/upstream-authority-cert-manager/pre-install.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 helm install cert-manager cert-manager --namespace cert-manager --create-namespace --version v1.11.0 --set installCRDs=true --repo https://charts.jetstack.io --wait
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 kubectl apply -f $SCRIPT_DIR/cert_manager_ca.yaml -n "$VALUES"
