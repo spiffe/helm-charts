833f59bc896567fe5fce35343ddd0dbb32bed89d
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-03-09 09:28:42 -0800

    Fix test.
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/upstream-authority-cert-manager/pre-install.sh
+++ .github/tests/upstream-authority-cert-manager/pre-install.sh
@@ -2,4 +2,4 @@
 
 helm install cert-manager cert-manager --namespace cert-manager --create-namespace --version v1.11.0 --set installCRDs=true --repo https://charts.jetstack.io --wait
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-kubectl apply -f $SCRIPT_DIR/cert-manager-ca.yaml -n "$VALUES"
+kubectl apply -f $SCRIPT_DIR/cert-manager-ca.yaml -n "$scenario"
