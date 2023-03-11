b6716aee177243f37401eee5a0350d569cf6f26d
kfox1111 <Kevin.Fox@pnnl.gov>
2023-03-11 03:11:19 -0800

    Test that it is possible to lock down security of pods (#84)

---
+++ .github/tests/lockdown/pre-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+kubectl label namespace "$scenario" pod-security.kubernetes.io/enforce=privileged
+kubectl create namespace "${scenario}-deps"
+kubectl label namespace "${scenario}-deps" pod-security.kubernetes.io/enforce=restricted
+helm install -n "${scenario}-deps" spire -f "${TEST_DIR}"/deps-values.yaml
