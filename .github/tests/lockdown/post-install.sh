4f881eba4f963a3fa0c57e39fe53fae53c6ab40c
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-03-12 12:30:49 -0700

    Add back debug logs
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/lockdown/post-install.sh
+++ .github/tests/lockdown/post-install.sh
@@ -17,3 +17,29 @@ cat <<EOF >>"$GITHUB_STEP_SUMMARY"
 | spire-agent | $("${k_rollout_status[@]}" spire-server daemonset spire-agent) |
 | spire-spiffe-oidc-discovery-provider | $("${k_wait[@]}" spire-server deployments.apps spire-spiffe-oidc-discovery-provider) |
 EOF
+
+if [ $1 -ne 0 ]; then
+  echo '==> Events of namespace spire-server'
+  echo '........................................................................................................................'
+  echo '>>> kubectl --request-timeout=30s get events --output wide --namespace spire-server'
+  kubectl --request-timeout=30s get events --output wide --namespace spire-server
+  echo '........................................................................................................................'
+  echo '<== Events of namespace spire-server'
+  echo '........................................................................................................................'
+  echo '>>> kubectl --request-timeout=30s describe pods --namespace spire-server'
+  kubectl --request-timeout=30s describe pods --namespace spire-server
+  echo '========================================================================================================================'
+  echo '==> Events of namespace spire-system'
+  echo '........................................................................................................................'
+  echo '>>> kubectl --request-timeout=30s get events --output wide --namespace spire-system'
+  kubectl --request-timeout=30s get events --output wide --namespace spire-system
+  echo '........................................................................................................................'
+  echo '<== Events of namespace spire-system'
+  echo '........................................................................................................................'
+  echo '>>> kubectl --request-timeout=30s describe pods --namespace spire-system'
+  kubectl --request-timeout=30s describe pods --namespace spire-system
+  echo '========================================================================================================================'
+  kubectl get pods -o name -n spire-server | while read line; do echo logs for $line; kubectl logs -n spire-server $line --all-containers=true --ignore-errors=true; done
+  kubectl get pods -o name -n spire-system | while read line; do echo logs for $line; kubectl logs -n spire-system $line --all-containers=true --ignore-errors=true; done
+  echo '========================================================================================================================'
+fi | cat >> "$GITHUB_STEP_SUMMARY"
