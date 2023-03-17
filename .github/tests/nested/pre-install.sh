8815872203523eb35bfbde8110695f55101bc0da
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-03-16 17:13:24 -0700

    Update nested spire to work with extra built in driver/agent
    This patch updates the nested spire work so that a separate deployment
    of the chart just for upstream support isn't needed.
    
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/nested/pre-install.sh
+++ .github/tests/nested/pre-install.sh
@@ -9,6 +9,3 @@ kubectl get all -n "${scenario}-root-server"
 kubectl get nodes -o go-template='{{range .items}}{{printf "%s\n" .metadata.uid}}{{end}}' | while read line; do
   kubectl exec -t spire-server-0 -n "${scenario}-root-server" -- spire-server entry create -spiffeID spiffe://example.org/example-cluster/nested-spire -parentID spiffe://example.org/spire/agent/k8s_psat/example-cluster/$line -selector k8s:pod-label:app.kubernetes.io/name:server -downstream
 done
-
-# Install just the upstream csi driver since it can't be done in the main deployment.
-helm install -n "${scenario}" spire-upstream-driver charts/spire --wait -f "${SCRIPT_DIR}/spire-upstream-driver-values.yaml"
