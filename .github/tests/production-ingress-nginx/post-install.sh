fed0d6c68c65ef38aaee8a83e648621187c6cdff
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-04-03 11:40:36 -0700

    Merge in new test functions
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/production-ingress-nginx/post-install.sh
+++ .github/tests/production-ingress-nginx/post-install.sh
@@ -5,17 +5,22 @@ set -x
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
-k_wait=(kubectl wait --for condition=available --timeout 30s --namespace)
-k_rollout_status=(kubectl rollout status --watch --timeout 30s --namespace)
+k_wait () {
+  kubectl wait --for condition=available --timeout 30s --namespace "$1" "$2" "$3" | tail -n 1
+}
+
+k_rollout_status () {
+  kubectl rollout status --watch --timeout 30s --namespace "$1" "$2" "$3" | tail -n 1
+}
 
 cat <<EOF >>"$GITHUB_STEP_SUMMARY"
 ### spire
 | workload | Status |
 | -------- | ------ |
-| spire-server | <pre>$("${k_rollout_status[@]}" spire-server statefulset spire-server)</pre> |
-| spire-spiffe-oidc-discovery-provider | <pre>$("${k_wait[@]}" spire-server deployments.apps spire-spiffe-oidc-discovery-provider)</pre> |
-| spire-spiffe-csi-driver | <pre>$("${k_rollout_status[@]}" spire-system daemonset spire-spiffe-csi-driver)</pre> |
-| spire-agent | <pre>$("${k_rollout_status[@]}" spire-system daemonset spire-agent)</pre> |
+| spire-server | <pre>$(k_rollout_status spire-server statefulset "${RELEASE}-server")</pre> |
+| spire-spiffe-csi-driver | <pre>$(k_rollout_status spire-system daemonset "${RELEASE}-spiffe-csi-driver")</pre> |
+| spire-agent | <pre>$(k_rollout_status spire-system daemonset "${RELEASE}-agent")</pre> |
+| spire-spiffe-oidc-discovery-provider | <pre>$(k_wait spire-server deployments.apps "${RELEASE}-spiffe-oidc-discovery-provider")</pre> |
 EOF
 
 if [ $1 -ne 0 ]; then
