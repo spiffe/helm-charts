4b3fa1097576febe50a734725c21f1379b5a8045
Mariusz Sabath <mrsabath@gmail.com>
2023-04-05 22:14:06 -0400

    Add helm test operation
    Signed-off-by: Mariusz Sabath <mrsabath@gmail.com>

--- .github/tests/tornjak/install.sh
+++ .github/tests/tornjak/install.sh
@@ -8,4 +8,5 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 helm install \
   --namespace spire-server \
   --values "${SCRIPTPATH}/../../../examples/tornjak/values.yaml" \
-  spire charts/spire
+  spire charts/spire --wait
+  helm test spire -n spire-server
