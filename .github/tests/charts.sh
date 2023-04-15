79b53985b49fc511f2e287a5f461341ca7f05184
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-04-15 15:07:35 -0700

    Fix the env
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/charts.sh
+++ .github/tests/charts.sh
@@ -1,2 +1,4 @@
-jq -r '.[] | ("HELM_REPO_" + .name | ascii_upcase | gsub("-";"_")) + "=" + .repo' .github/tests/charts.json  | tr '-' '_' | eval
-jq -r '.[] | ("VERSION_" + .name | ascii_upcase | gsub("-";"_")) + "=" + .version' .github/tests/charts.json  | tr '-' '_' | eval
+REPOS=$(jq -r '.[] | "export " + ("HELM_REPO_" + .name | ascii_upcase | gsub("-";"_")) + "=" + .repo' .github/tests/charts.json  | tr '-' '_')
+VERSIONS=$(jq -r '.[] | "export " + ("VERSION_" + .name | ascii_upcase | gsub("-";"_")) + "=" + .version' .github/tests/charts.json  | tr '-' '_')
+eval "$REPOS"
+eval "$VERSIONS"
