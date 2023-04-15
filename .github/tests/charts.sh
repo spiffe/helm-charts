059bdb370b17becebe95544699bafce63e41bf3f
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-04-15 15:35:51 -0700

    Fix formatting
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/charts.sh
+++ .github/tests/charts.sh
@@ -1,4 +1,4 @@
-REPOS=$(jq -r '.[] | "export " + ("HELM_REPO_" + .name | ascii_upcase | gsub("-";"_")) + "=" + .repo' .github/tests/charts.json  | tr '-' '_')
-VERSIONS=$(jq -r '.[] | "export " + ("VERSION_" + .name | ascii_upcase | gsub("-";"_")) + "=" + .version' .github/tests/charts.json  | tr '-' '_')
+REPOS=$(jq -r '.[] | "export " + ("HELM_REPO_" + .name | ascii_upcase | gsub("-";"_")) + "=" + .repo' .github/tests/charts.json)
+VERSIONS=$(jq -r '.[] | "export " + ("VERSION_" + .name | ascii_upcase | gsub("-";"_")) + "=" + .version' .github/tests/charts.json)
 eval "$REPOS"
 eval "$VERSIONS"
