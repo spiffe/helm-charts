e13b73c8b176c20d136a1cd76b03377cefd0acab
Marco Franssen <marco.franssen@gmail.com>
2023-04-24 17:35:05 +0200

    Resolve shellcheck issues
    Signed-off-by: Marco Franssen <marco.franssen@gmail.com>

--- .github/workflows/scripts/update-versions.sh
+++ .github/workflows/scripts/update-versions.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
 CHARTJSON=$SCRIPTPATH/../../tests/charts.json
 
-jq -r ".[].name" $CHARTJSON | while read CHART; do
+jq -r ".[].name" "${CHARTJSON}" | while read -r CHART; do
   ENTRYQUERY='.[] | select(.name == "'$CHART'")'
-  REPO_URL="$(jq -r "$ENTRYQUERY | .repo" $CHARTJSON)"
-  VERSION="$(jq -r "$ENTRYQUERY | .version" $CHARTJSON)"
-  echo Processing: $CHART
+  REPO_URL="$(jq -r "$ENTRYQUERY | .repo" "${CHARTJSON}")"
+  VERSION="$(jq -r "$ENTRYQUERY | .version" "${CHARTJSON}")"
+  echo Processing: ${CHART}
   echo "  repo: $REPO_URL"
   echo "  current version: $VERSION"
   helm repo add "$CHART" "$REPO_URL" > /dev/null
@@ -18,7 +18,7 @@ jq -r ".[].name" $CHARTJSON | while read CHART; do
   echo "  latest version: $LATEST_VERSION"
   if [ "x$VERSION" != "x$LATEST_VERSION" ]; then
     echo "  New version found!"
-    jq "( $ENTRYQUERY ).version |= "'"'$LATEST_VERSION'"' $CHARTJSON > /tmp/$$
-    mv /tmp/$$ $CHARTJSON
+    jq "( $ENTRYQUERY ).version |= "'"${LATEST_VERSION}"' "${CHARTJSON}" > /tmp/$$
+    mv /tmp/$$ "${CHARTJSON}"
   fi
 done
