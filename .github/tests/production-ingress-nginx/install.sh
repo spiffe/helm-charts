f2dece26d0ae03dc1a13afdf11d8c7918302eec6
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-04-26 07:57:54 -0700

    Fix shellcheck issues
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/production-ingress-nginx/install.sh
+++ .github/tests/production-ingress-nginx/install.sh
@@ -10,14 +10,14 @@ kubectl create namespace spire-server
 kubectl label namespace spire-server pod-security.kubernetes.io/enforce=restricted
 
 helm install cert-manager cert-manager --namespace cert-manager --create-namespace --version "$VERSION_CERT_MANAGER" --set installCRDs=true --repo "$HELM_REPO_CERT_MANAGER" --wait
-kubectl apply -f $SCRIPT_DIR/testcert.yaml -n spire-server
+kubectl apply -f "$SCRIPT_DIR/testcert.yaml" -n spire-server
 
 helm install ingress-nginx ingress-nginx --version "$VERSION_INGRESS_NGINX" --repo "$HELM_REPO_INGRESS_NGINX" --create-namespace -n ingress-nginx --wait \
     --set controller.extraArgs.enable-ssl-passthrough=,controller.admissionWebhooks.enabled=false,controller.service.type=ClusterIP \
     --set controller.ingressClassResource.default=true
 
 ip=$(kubectl get svc -n ingress-nginx ingress-nginx-controller -o go-template='{{ .spec.clusterIP }}')
-echo $ip oidc-discovery.example.org
+echo "$ip" oidc-discovery.example.org
 
 cat > /tmp/dummydns <<EOF
 spiffe-oidc-discovery-provider:
--- .github/tests/production-ingress-nginx/post-install.sh
+++ .github/tests/production-ingress-nginx/post-install.sh
@@ -2,43 +2,17 @@
 
 set -x
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPT="$(readlink -f "$0")"
+SCRIPTPATH="$(dirname "${SCRIPT}")"
+scenario="${scenario:-$(basename "${SCRIPTPATH}")}"
 
-k_wait () {
-  kubectl wait --for condition=available --timeout 30s --namespace "$1" "$2" "$3" | tail -n 1
-}
+# shellcheck source=/dev/null
+source "${SCRIPTPATH}/../common.sh"
 
-k_rollout_status () {
-  kubectl rollout status --watch --timeout 30s --namespace "$1" "$2" "$3" | tail -n 1
-}
+print_helm_releases
+print_spire_workload_status spire-server spire-system
 
-cat <<EOF >>"$GITHUB_STEP_SUMMARY"
-### spire
-| workload | Status |
-| -------- | ------ |
-| spire-server | <pre>$(k_rollout_status spire-server statefulset "${RELEASE}-server")</pre> |
-| spire-spiffe-csi-driver | <pre>$(k_rollout_status spire-system daemonset "${RELEASE}-spiffe-csi-driver")</pre> |
-| spire-agent | <pre>$(k_rollout_status spire-system daemonset "${RELEASE}-agent")</pre> |
-| spire-spiffe-oidc-discovery-provider | <pre>$(k_wait spire-server deployments.apps "${RELEASE}-spiffe-oidc-discovery-provider")</pre> |
-EOF
-
-if [ $1 -ne 0 ]; then
-  for ns in spire-server spire-system ingress-nginx cert-manager; do
-    echo
-    echo '```'
-    echo "==> Events of namespace $ns"
-    echo "........................................................................................................................"
-    echo ">>> kubectl --request-timeout=30s get events --output wide --namespace $ns"
-    kubectl --request-timeout=30s get events --output wide --namespace $ns
-    echo "........................................................................................................................"
-    echo "<== Events of namespace $ns"
-    echo "........................................................................................................................"
-    echo ">>> kubectl --request-timeout=30s describe pods --namespace $ns"
-    kubectl --request-timeout=30s describe pods --namespace $ns
-    echo "========================================================================================================================"
-    kubectl get pods -o name -n $ns | while read line; do echo logs for $line; kubectl logs -n $ns $line --all-containers=true --ignore-errors=true; done
-    echo '========================================================================================================================'
-    echo '```'
-  done
-fi | cat >> "$GITHUB_STEP_SUMMARY"
+if [[ "$1" -ne 0 ]]; then
+  get_namespace_details spire-server
+  get_namespace_details spire-system
+fi
