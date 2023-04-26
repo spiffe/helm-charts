90c42af38922d9cbc2150908883d675eae121be3
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-04-26 07:19:25 -0700

    Fix merge issue
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/production-external-postgresql/post-install.sh
+++ .github/tests/production-external-postgresql/post-install.sh
@@ -2,50 +2,17 @@
 
 set -x
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "$SCRIPT")
+SCRIPT="$(readlink -f "$0")"
+SCRIPTPATH="$(dirname "${SCRIPT}")"
+scenario="${scenario:-$(basename "${SCRIPTPATH}")}"
 
-k_wait=(kubectl wait --for condition=available --timeout 30s --namespace)
-k_rollout_status=(kubectl rollout status --watch --timeout 30s --namespace)
+# shellcheck source=/dev/null
+source "${SCRIPTPATH}/../common.sh"
 
-function get_namespace_details {
-cat <<EOF >>"$GITHUB_STEP_SUMMARY"
-### Namespace $1
+print_helm_releases
+print_spire_workload_status spire-server spire-system
 
-#### Events
-
-\`\`\`shell
-$(kubectl --request-timeout=30s get events --output wide --namespace "$1")
-\`\`\`
-
-#### Pods
-
-\`\`\`shell
-$(kubectl --request-timeout=30s describe pods --namespace "$1")
-\`\`\`
-
-#### Logs
-
-\`\`\`shell
-$(kubectl get pods -o name -n "$1" | while read -r line; do echo logs for "${line}"; kubectl logs -n "$1" "${line}" --all-containers=true --ignore-errors=true; done)
-\`\`\`
-
-EOF
-}
-
-cat <<EOF >>"$GITHUB_STEP_SUMMARY"
-### spire
-
-| workload                             | Status |
-| ------------------------------------ | ------ |
-| spire-server                         | "$("${k_rollout_status[@]}" spire-server statefulset spire-server)" |
-| spire-controller-manager             | "$("${k_rollout_status[@]}" spire-server statefulset spire-controller-manager)" |
-| spire-spiffe-oidc-discovery-provider | "$("${k_wait[@]}" spire-server deployments.apps spire-spiffe-oidc-discovery-provider)" |
-| spire-spiffe-csi-driver              | "$("${k_rollout_status[@]}" spire-system daemonset spire-spiffe-csi-driver)" |
-| spire-agent                          | "$("${k_rollout_status[@]}" spire-system daemonset spire-agent)" |
-EOF
-
-if [ $1 -ne 0 ]; then
+if [[ "$1" -ne 0 ]]; then
   get_namespace_details spire-server
-  get_namespace_details spire-systen
+  get_namespace_details spire-system
 fi
