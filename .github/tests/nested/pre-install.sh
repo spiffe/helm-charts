26c0fd9ae4a86cadeaf59d32f1e2cb1de2c8a4d4
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-04-26 07:55:53 -0700

    Fix shellcheck issues
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/tests/nested/pre-install.sh
+++ .github/tests/nested/pre-install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-SCRIPT_DIR=$(dirname "$0")
+SCRIPT="$(readlink -f "$0")"
+SCRIPTPATH="$(dirname "${SCRIPT}")"
+scenario="${scenario:-$(basename "${SCRIPTPATH}")}"
 
 kubectl create namespace "${scenario}-root-server"
 # Install a spire root server for testing against.
 helm install -n "${scenario}-root-server" spire charts/spire --wait -f "${SCRIPT_DIR}/spire-root-server-values.yaml"
 kubectl get all -n "${scenario}-root-server"
-kubectl get nodes -o go-template='{{range .items}}{{printf "%s\n" .metadata.uid}}{{end}}' | while read line; do
-  kubectl exec -t spire-server-0 -n "${scenario}-root-server" -- spire-server entry create -spiffeID spiffe://example.org/example-cluster/nested-spire -parentID spiffe://example.org/spire/agent/k8s_psat/example-cluster/$line -selector k8s:pod-label:app.kubernetes.io/name:server -downstream
+kubectl get nodes -o go-template='{{range .items}}{{printf "%s\n" .metadata.uid}}{{end}}' | while read -r line; do
+  kubectl exec -t spire-server-0 -n "${scenario}-root-server" -- spire-server entry create -spiffeID spiffe://example.org/example-cluster/nested-spire -parentID "spiffe://example.org/spire/agent/k8s_psat/example-cluster/$line" -selector k8s:pod-label:app.kubernetes.io/name:server -downstream
 done
