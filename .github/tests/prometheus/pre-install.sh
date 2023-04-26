aabe77f6e306e048ae979e1e2f744faf61207c12
Marco Franssen <marco.franssen@gmail.com>
2023-04-26 11:51:31 +0200

    Add post-install scripts to all tests to capture failure details
    Signed-off-by: Marco Franssen <marco.franssen@gmail.com>

--- .github/tests/prometheus/pre-install.sh
+++ .github/tests/prometheus/pre-install.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "${SCRIPT}")
+SCRIPT="$(readlink -f "$0")"
+SCRIPTPATH="$(dirname "${SCRIPT}")"
 scenario="${scenario:-$(basename "${SCRIPTPATH}")}"
 
-helm install kube-prometheus-stack kube-prometheus-stack --version "${VERSION_KUBE_PROMETHEUS_STACK}" --repo "${HELM_REPO_KUBE_PROMETHEUS_STACK}" -n "${scenario}" --wait
+helm install kube-prometheus-stack kube-prometheus-stack \
+  --version "${VERSION_KUBE_PROMETHEUS_STACK}" \
+  --repo "${HELM_REPO_KUBE_PROMETHEUS_STACK}" \
+  -n "${scenario}" \
+  --wait
--- .github/tests/spire-oidc-insecure/pre-install.sh
+++ .github/tests/spire-oidc-insecure/pre-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-SCRIPT=$(readlink -f "$0")
-SCRIPTPATH=$(dirname "${SCRIPT}")
+SCRIPT="$(readlink -f "$0")"
+SCRIPTPATH="$(dirname "${SCRIPT}")"
 scenario="${scenario:-$(basename "${SCRIPTPATH}")}"
 
 helm install ingress-nginx ingress-nginx --version "${VERSION_INGRESS_NGINX}" --repo "${HELM_REPO_INGRESS_NGINX}" -n "$scenario" --set controller.extraArgs.enable-ssl-passthrough=
