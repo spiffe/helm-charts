8a80e2b5b00b20eff7d947aee6193500bdae22e8
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-04-30 16:20:09 -0700

    Fix script
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- update-deps.sh
+++ update-deps.sh
@@ -4,7 +4,7 @@ SCRIPT="$(readlink -f "$0")"
 SCRIPTPATH="$(dirname "${SCRIPT}")"
 
 for chart in spiffe-csi-driver spiffe-oidc-discovery-provider spire-agent spire-server spire; do
-  pushd "charts/${chart}"
+  pushd "${SCRIPTPATH}/charts/${chart}" || exit 1
   helm dep up
-  popd
+  popd || exit 1
 done
