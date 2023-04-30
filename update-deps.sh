99952aa12ae9a93615f254d1cf3c00874e5060b3
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-04-30 16:15:53 -0700

    Add missing script
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

---
+++ update-deps.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT="$(readlink -f "$0")"
+SCRIPTPATH="$(dirname "${SCRIPT}")"
+
+for chart in spiffe-csi-driver spiffe-oidc-discovery-provider spire-agent spire-server spire; do
+  pushd "charts/${chart}"
+  helm dep up
+  popd
