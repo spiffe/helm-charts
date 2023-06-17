79b08ffdc73a6b8e2dbf5326d51fbaad09090145
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-06-17 07:16:54 -0700

    Incorperate feedback. Deal with linewarpping and nulls.
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- .github/scripts/edit-yaml.py
+++ .github/scripts/edit-yaml.py
@@ -5,9 +5,15 @@ import sys
 from dict_deep import deep_set
 import ruamel.yaml
 
+def represent_none(self, data):
+    return self.represent_scalar(u'tag:yaml.org,2002:null', u'null')
+
 y = ruamel.yaml.YAML()
 y.indent(mapping=2, sequence=4, offset=2)
+# Dont wrap long lines
+y.width = 4096
 y.preserve_quotes = True
+y.representer.add_representer(type(None), represent_none)
 
 d = y.load(open(os.environ['VALUES']))
 
