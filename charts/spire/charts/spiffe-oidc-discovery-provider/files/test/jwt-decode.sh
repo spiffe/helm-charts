f6bff53bd15c72d11ac17d49ed1024d78a07469f
Drew Wells <dwells@infoblox.com>
2023-08-16 11:49:14 -0500

    whitespace
    Signed-off-by: Drew Wells <dwells@infoblox.com>

--- charts/spire/charts/spiffe-oidc-discovery-provider/files/test/jwt-decode.sh
+++ charts/spire/charts/spiffe-oidc-discovery-provider/files/test/jwt-decode.sh
@@ -1,4 +1,6 @@
-#!/bin/bash -x
+#!/bin/bin/env
+
+set -x
 
 # https://gist.github.com/stokito/f2d7ea0b300f14638a9063559384ec89/
 # Decode a JWT from stdin and verify it's signature with the JWT issuer public key
@@ -14,13 +16,12 @@ URL=$1
 JWT=$2
 
 if [ -z "$(command -v jq)" ]; then
-    echo "This script will NOT work on your machine."
-    echo "Please install jq first: https://stedolan.github.io/jq/download/"
-    exit 1
+  echo "This script will NOT work on your machine."
+  echo "Please install jq first: https://stedolan.github.io/jq/download/"
+  exit 1
 fi
 
-base64_padding()
-{
+base64_padding() {
   local len=$(( ${#1} % 4 ))
   local padded_b64=''
   if [ ${len} = 2 ]; then
@@ -33,32 +34,31 @@ base64_padding()
   echo -n "$padded_b64"
 }
 
-base64url_to_b64()
-{
+base64url_to_b64() {
   base64_padding "${1}" | tr -- '-_' '+/'
 }
 
 b2hex() { echo -n "$1"==== | fold -w 4 | sed '$ d' | tr -d '\n' |base64 -d | xxd -p | tr -d \\n; }
 
 mint_rsa_key() {
-    JWK=$1
+  JWK=$1
 
-    # Extract the modulus and exponent from the JWK, converting from URL-safe Base64 to standard Base64
-    MODULUS=$(echo "$JWK" | jq -r '.n' | tr '_-' '/+')
-    EXPONENT=$(echo "$JWK" | jq -r '.e' | tr '_-' '/+')
+  # Extract the modulus and exponent from the JWK, converting from URL-safe Base64 to standard Base64
+  MODULUS=$(echo "$JWK" | jq -r '.n' | tr '_-' '/+')
+  EXPONENT=$(echo "$JWK" | jq -r '.e' | tr '_-' '/+')
 
-    modulus=$(b2hex "$MODULUS")
-    exponent=$(b2hex "$EXPONENT")
+  modulus=$(b2hex "$MODULUS")
+  exponent=$(b2hex "$EXPONENT")
 
-    asnconf=$(mktemp)
+  asnconf=$(mktemp)
 
-    asnconf="asn1=SEQUENCE:pubkeyinfo\n[pubkeyinfo]\nalgorithm=SEQUENCE:rsa_alg\npubkey=BITWRAP,SEQUENCE:rsapubkey\n[rsa_alg]\nalgorithm=OID:rsaEncryption\nparameter=NULL\n[rsapubkey]\nn=INTEGER:0x$modulus\ne=INTEGER:0x$exponent"
+  asnconf="asn1=SEQUENCE:pubkeyinfo\n[pubkeyinfo]\nalgorithm=SEQUENCE:rsa_alg\npubkey=BITWRAP,SEQUENCE:rsapubkey\n[rsa_alg]\nalgorithm=OID:rsaEncryption\nparameter=NULL\n[rsapubkey]\nn=INTEGER:0x$modulus\ne=INTEGER:0x$exponent"
 
-    derfile=$(mktemp)
-    echo >&2 "derfile: $derfile"
-    echo -e "$asnconf" | openssl asn1parse -genconf /dev/stdin -noout -out "$derfile"
+  derfile=$(mktemp)
+  echo >&2 "derfile: $derfile"
+  echo -e "$asnconf" | openssl asn1parse -genconf /dev/stdin -noout -out "$derfile"
 
-    openssl rsa -in "$derfile" -inform DER -pubin
+  openssl rsa -in "$derfile" -inform DER -pubin
 }
 
 # read the JWT from stdin and split by comma into three variables
@@ -99,18 +99,18 @@ mint_rsa_key "$JWK" > "$PUB_KEY_FILE"
 
 # verify signature
 if [ "${JWT_ALG}" = "RS256" ]; then
-    #SIG_FILE="/tmp/$JWT_SUB-$JWT_IAT.sig.dat"
-    SIG_FILE=$(mktemp)
-    echo -n "$JWT_SIGNATURE_B64" | base64 -d > "${SIG_FILE}"
-    JWT_BODY=$(echo -n "$JWT_HEADER_B64URL.$JWT_PAYLOAD_B64URL")
-    echo -n "$JWT_BODY" | openssl dgst -sha256 -verify "${PUB_KEY_FILE}" -signature "${SIG_FILE}"
-    JWT_SIG_VERIFIED=$?
-    rm "${SIG_FILE}"
-    if [ ${JWT_SIG_VERIFIED} -ne 0 ]; then
-      >&2 echo "Bad Signature"
-      exit ${JWT_SIG_VERIFIED};
-    fi
+  #SIG_FILE="/tmp/$JWT_SUB-$JWT_IAT.sig.dat"
+  SIG_FILE=$(mktemp)
+  echo -n "$JWT_SIGNATURE_B64" | base64 -d > "${SIG_FILE}"
+  JWT_BODY=$(echo -n "$JWT_HEADER_B64URL.$JWT_PAYLOAD_B64URL")
+  echo -n "$JWT_BODY" | openssl dgst -sha256 -verify "${PUB_KEY_FILE}" -signature "${SIG_FILE}"
+  JWT_SIG_VERIFIED=$?
+  rm "${SIG_FILE}"
+  if [ ${JWT_SIG_VERIFIED} -ne 0 ]; then
+    >&2 echo "Bad Signature"
+    exit ${JWT_SIG_VERIFIED};
+  fi
 else
-    >&2 echo "Error 3: Unsupported signature algorithm $JWT_ALG"
-    exit 3
+  >&2 echo "Error 3: Unsupported signature algorithm $JWT_ALG"
+  exit 3
 fi
