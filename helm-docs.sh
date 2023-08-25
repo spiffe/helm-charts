cf54cb4200b2961c23aa0d850b835979055525f5
Krishnakumar Venkataraman <krishna_kumar08@infosys.com>
2023-08-25 21:30:30 +0530

    Ensure npm is installed in helm-docs.sh script
    Signed-off-by: Krishnakumar Venkataraman <krishna_kumar08@infosys.com>

--- helm-docs.sh
+++ helm-docs.sh
@@ -6,21 +6,23 @@ SCRIPTPATH=$(dirname "$0")
 README_GENERATOR_VERSION="2.5.1"
 README_GENERATOR_EXE="readme-generator"
 
-if [[ -z '$(type -d "${README_GENERATOR_EXE}")' ]]; then
-  echo "readme-generator not installed successfully!!"
+
+if [[ -z '$(type "${README_GENERATOR_EXE}")' ]]; then
+  echo "readme-generator not installed. Installing..."
+  if [[ -z '$(type npm)' ]]; then
+    echo "npm is required to install ${README_GENERATOR_EXE}. Please install npm and rerun the script."
+    exit 1
+  fi
   # platform agnostic npm install, also adds into the path
   npm install -g "@bitnami/readme-generator-for-helm@${README_GENERATOR_VERSION}"
 fi
 
 # generate docs and show the diff
-
 chart_paths=( $( find -type f -iname "Chart.yaml" | xargs -L1 dirname ) )
 for cpath in "${chart_paths[@]}"
 do
   echo "readme-generator --values=${cpath}/values.yaml --readme=${cpath}/README.md"
   readme-generator --values="${cpath}/values.yaml" --readme="${cpath}/README.md"
 done
-
-# readme-generator --values="${SCRIPTPATH}/charts/spire/values.yaml" --readme="${SCRIPTPATH}/charts/spire/README.md"
-# git diff --exit-code
+git diff --exit-code
 
