5273f4e53022064ceeefe3828a0a58d5d27484b4
kfox1111 <Kevin.Fox@pnnl.gov>
2023-09-01 12:25:07 -0700

    Switch mysql and postgresql tests to HA Production configs (#471)
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- examples/external-postgresql/run-tests.sh
+++ examples/external-postgresql/run-tests.sh
@@ -12,31 +12,35 @@ source "${SCRIPTPATH}/../../.github/scripts/parse-versions.sh"
 # shellcheck source=/dev/null
 source "${TESTDIR}/common.sh"
 
-helm_install=(helm upgrade --install --create-namespace)
-ns=spire-system
-
 teardown() {
-  helm uninstall --namespace "${ns}" spire 2>/dev/null || true
-  kubectl delete ns "${ns}" 2>/dev/null || true
-
-  helm uninstall --namespace postgresql postgresql 2>/dev/null || true
-  kubectl delete ns postgresql 2>/dev/null || true
+  helm uninstall --namespace "spire-server" spire 2>/dev/null || true
+  helm uninstall --namespace "spire-server" postgresql 2>/dev/null || true
+  kubectl delete ns spire-server 2>/dev/null || true
+  kubectl delete ns spire-system 2>/dev/null || true
 }
 
 trap 'trap - SIGTERM && teardown' SIGINT SIGTERM EXIT
 
-"${helm_install[@]}" postgresql postgresql --version "$VERSION_POSTGRESQL" --repo "$HELM_REPO_POSTGRESQL" \
-  --namespace postgresql \
-  --values "${DEPS}/postgresql.yaml" \
+kubectl create namespace spire-system --dry-run=client -o yaml | kubectl apply -f -
+kubectl label namespace spire-system pod-security.kubernetes.io/enforce=privileged || true
+kubectl create namespace spire-server --dry-run=client -o yaml | kubectl apply -f -
+kubectl label namespace spire-server pod-security.kubernetes.io/enforce=restricted || true
+
+helm upgrade --install postgresql postgresql --version "$VERSION_POSTGRESQL" --repo "$HELM_REPO_POSTGRESQL" \
+  --namespace spire-server \
+  --values "${DEPS}/postgresql.yaml,${SCRIPTPATH}/../production/values.yaml,${SCRIPTPATH}/../production/values-node-pod-antiaffinity.yaml" \
   --wait
 
-"${helm_install[@]}" --namespace "${ns}" --values "${SCRIPTPATH}/values.yaml" \
+helm upgrade --install --namespace "spire-server" \
+  --values "${SCRIPTPATH}/values.yaml,${SCRIPTPATH}/../production/values.yaml,${SCRIPTPATH}/../production/values-node-pod-antiaffinity.yaml" \
   --set 'spire-server.dataStore.sql.password=sp1ff3Test' --wait spire charts/spire
-helm test --namespace "${ns}" spire
+helm test --namespace "spire-server" spire
 
 print_helm_releases
-print_spire_workload_status "${ns}"
+print_spire_workload_status spire-server
+print_spire_workload_status spire-system
 
 if [[ "$1" -ne 0 ]]; then
-  get_namespace_details "${ns}"
+  get_namespace_details spire-server
+  get_namespace_details spire-system
 fi
