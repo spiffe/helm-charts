18b046f40abe2042cca691496a0a9e3234b43fa6
Kevin Fox <Kevin.Fox@pnnl.gov>
2023-09-11 10:05:46 -0700

    Docs need to ignore spire-lib
    Signed-off-by: Kevin Fox <Kevin.Fox@pnnl.gov>

--- helm-docs.sh
+++ helm-docs.sh
@@ -14,7 +14,7 @@ if ! hash "${README_GENERATOR_EXE}" 2>/dev/null; then
 fi
 
 # generate docs and show the diff
-mapfile -t chart_paths < <(find "$SCRIPTPATH/charts" -type f -iname "Chart.yaml" -exec dirname {} +)
+mapfile -t chart_paths < <(find "$SCRIPTPATH/charts" -type f -iname "Chart.yaml" ! -name "*spire-lib/Chart.yaml" -exec dirname {} +)
 for cpath in "${chart_paths[@]}"
 do
   echo >&2 "Generating Chart documentation for ${cpath}…"
